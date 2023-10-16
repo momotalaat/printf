357cdde0e1d67676fc21adc8fa7a1201b65d9acb
momotalaat <mgivermgiver@yahoo.com>
2023-10-16 01:51:03 -0700

    task.0

---
+++ test/main.h
@@ -0,0 +1,24 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdarg.h>
+#include <unistd.h>
+#include <limits.h>
+
+
+
+
+int _putchar(char c);
+int _printf(const char *format, ...);
+
+
+
+
+
+
+
+
+#endif
